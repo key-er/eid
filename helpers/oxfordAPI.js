449a5800ff70354669f5d7757d85b8591d657f31
key-er <email@example.com>
2018-06-14 17:35:12 -0700

    draft api fetch

---
+++ helpers/oxfordAPI.js
@@ -0,0 +1,29 @@
+const request = require('request');
+const config = require('../config.js')
+
+let options = {
+  url: 'https://od-api.oxforddictionaries.com/api/v1/entries/en/happy/synonyms;antonyms',
+  headers: {
+      'User-Agent': 'request',
+      'app_id': config.APP_ID,
+      'app_key': config.APP_KEY,
+    }
+}
+
+
+
+
+request(options, function(err, res, body) {
+  if (err) console.log("ERRRR", err)
+  else {
+    var jData = JSON.parse(body)
+    console.log(jData.results[0].word)
+    console.log(jData.results[0].lexicalEntries[0].entries[0].senses[0].synonyms)
+
+    console.log(jData.results[0].lexicalEntries[0].entries[0].senses[0].antonyms)
+
+    console.log(jData.results[0].lexicalEntries[0].entries[0].senses[0].examples)
+
+    // console.log(body.results[0].lexicalEntries[0].entries[0].senses[0].synonyms)
+  }
+  });
